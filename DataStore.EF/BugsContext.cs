f398d036c2cffd29705e7809b361a4608258892a
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-09-25 19:17:03 +0300

    add entity framework API

---
+++ DataStore.EF/BugsContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Core.Models;
+
+namespace DataStore.EF
+{
+    public class BugsContext : DbContext
+    {
+        public BugsContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>()
+                .HasMany(p => p.Tickets)
+                .WithOne(t => t.Project)
+                .HasForeignKey(t => t.ProjectId);
+
+            //seeding
+            modelBuilder.Entity<Project>().HasData(
+                new Project { Id = 1, Name = "Project 1" },
+                new Project { Id = 2, Name = "Project 2" }
+                );
+
+            modelBuilder.Entity<Ticket>().HasData(
+                new Ticket { Id = 1, Title = "Bug #1", ProjectId = 1 },
+                new Ticket { Id = 2, Title = "Bug #2", ProjectId = 1 },
+                new Ticket { Id = 3, Title = "Bug #3", ProjectId = 2 }
+                );
+        }
+    }
+}
--- TicketsAPI/Program.cs
+++ TicketsAPI/Program.cs
@@ -1,10 +1,26 @@
 global using Core.Models;
+using DataStore.EF;
+using Microsoft.EntityFrameworkCore;
+using TicketsAPI.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<BugsContext>(options =>
+    {
+        options.UseInMemoryDatabase("Bugs");
+    });
+}
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.CreateDb();
+}
 
 app.MapControllers();
 
