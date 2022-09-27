5413d5d6555500d9410b6298caa2dca4f2b23182
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-09-27 21:19:55 +0300

    configure v1 for open api

--- TicketsAPI/Program.cs
+++ TicketsAPI/Program.cs
@@ -22,16 +22,29 @@ builder.Services.AddApiVersioning(options =>
     options.ReportApiVersions = true;
     options.AssumeDefaultVersionWhenUnspecified = true;
     options.DefaultApiVersion = new ApiVersion(1, 0);
-    //options.ApiVersionReader = new HeaderApiVersionReader("x-API-Version");
-
+    options.ApiVersionReader = new HeaderApiVersionReader("x-API-Version");
 });
+
+builder.Services.AddVersionedApiExplorer(options => 
+{
+    options.GroupNameFormat = "'v'VVV";
+});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if(app.Environment.IsDevelopment())
 {
     app.CreateDb();
+
 }
+
+//Configure openAPI
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+});
 
 app.MapControllers();
 
