4a9259e1d01600a3bb356422aa20090054d6d7d1
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-10-05 21:05:36 +0300

    add auth classes and methods to project for custom authorization

---
+++ TicketsAPI/Auth/CustomUserManager.cs
@@ -0,0 +1,22 @@
+﻿namespace TicketsAPI.Auth
+{
+    public class CustomUserManager
+    {
+        private Dictionary<string, string> credentials = new Dictionary<string, string>() 
+        {
+            {"Brian", "password1"}
+        };
+        private readonly ICustomTokenManager _customTokenManager;
+
+        public CustomUserManager(ICustomTokenManager customTokenManager)
+        {
+            _customTokenManager = customTokenManager;
+        }
+
+        public string Authenticate(string username, string password)
+        {
+            if (credentials[username] != password) return string.Empty;
+            return _customTokenManager.CreateToken(username);
+        }
+    }
+}
